45ee2a989ce17b069d773a0dba70b8be15f52409
ghjgtghjfgj <1587042786@qq.com>
2022-05-30 21:48:05 +0800

    test： 节气完成

--- src/team/lunar_solar/Animal_year.java
+++ src/team/lunar_solar/Animal_year.java
@@ -19,14 +19,6 @@ public class Animal_year {
      */
 
     static  public String  Solar_Animal(int year,int month,int monthDay){
-        if (year < MIN_YEAR || year > MAX_YEAR || month < 1 || month > 12
-                || monthDay < 1 || monthDay > 30) {
-            throw new IllegalArgumentException(//若输入的参数超过上下限，则抛出不合法参数异常
-                    "Illegal lunar date, must be like that:\n\t" +
-                            "year : 1900~2099\n\t" +
-                            "month : 1~12\n\t" +
-                            "day : 1~30");
-        }
 
         int[] lunardate=LS.solarToLunar(year,month,monthDay);
         return  Lunar_Animal(lunardate[0],lunardate[1],lunardate[2]);
